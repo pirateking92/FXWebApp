29e6893e03c1c84ac9ccf34ee89a0a914453411f
Matt Doyle <doyle9214@gmail.com>
2024-09-11 15:44:43 +0100

    getting back to basic python. write tests for the api response and input

--- backend/fx_backend.py
+++ backend/fx_backend.py
@@ -1,2 +1,15 @@
-def fx_backend():
-    pass
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+headers = os.getenv('api_key')
+currencies = input(str("What currency?: "))
+
+url = f"https://api.currencyapi.com/v3/latest?apikey={headers}&currencies={currencies}"
+
+response = requests.get(url)
+print(headers)
+print(url)
+print(response.text)
